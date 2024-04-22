cafe227b5d08d190228d27cd24a469b70720bcdd
David Eduardo Rueda Salgado <david.rueda.salgado@udla.edu.ec>
2024-04-22 07:57:56 -0500

    Creacion del proyecto y modelos

---
+++ ExamenP1_DRueda/Models/Carrera.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenP1_DRueda.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int id_carrera { get; set; }
+        public string nombre_carrera { get; set; }
+        public string campus { get; set; }
+        public int numero_semestres { get; set; }
+    }
+}
---
+++ ExamenP1_DRueda/Models/DRueda.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenP1_DRueda.Models
+{
+    public class DRueda
+    {
+        public int id { get; set; }
+        public string? Nombre { get; set; }
+        public decimal? pagoMatricula { get; set; }
+        public bool? IsMatriculado { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
