e551e5c1ae7698d6eabab29eb2b4c9c8a335f680
David Eduardo Rueda Salgado <david.rueda.salgado@udla.edu.ec>
2024-04-22 08:12:04 -0500

    Agregado las restricciones de Data Annotation

--- ExamenP1_DRueda/Models/Carrera.cs
+++ ExamenP1_DRueda/Models/Carrera.cs
@@ -5,9 +5,17 @@ namespace ExamenP1_DRueda.Models
     public class Carrera
     {
         [Key]
-        public int id_carrera { get; set; }
+        public int IdCarrera { get; set; }
+        [Required(ErrorMessage = "El nombre de la carrera es requerido.")]
+        [StringLength(100, ErrorMessage = "El nombre de la carrera no puede exceder los 100 caracteres.")]
         public string nombre_carrera { get; set; }
+
+        [Required(ErrorMessage = "El campus es requerido.")]
+        [StringLength(50, ErrorMessage = "El campus no puede exceder los 50 caracteres.")]
         public string campus { get; set; }
+
+        [Range(1, 10, ErrorMessage = "El número de semestres debe estar entre 1 y 10.")]
         public int numero_semestres { get; set; }
     }
+
 }
--- ExamenP1_DRueda/Models/DRueda.cs
+++ ExamenP1_DRueda/Models/DRueda.cs
@@ -1,11 +1,24 @@
-﻿namespace ExamenP1_DRueda.Models
+﻿using ExamenP1_DRueda.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class DRueda
 {
-    public class DRueda
-    {
-        public int id { get; set; }
-        public string? Nombre { get; set; }
-        public decimal? pagoMatricula { get; set; }
-        public bool? IsMatriculado { get; set; }
-        public DateTime? Fecha { get; set; }
-    }
+    public int id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es requerido.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "El pago de matrícula es requerido.")]
+    [Range(0, 10000, ErrorMessage = "El pago de matrícula debe estar entre 0 y 10000.")]
+    public decimal? pagoMatricula { get; set; }
+
+    [Required(ErrorMessage = "Es necesario indicar si está matriculado.")]
+    public bool? IsMatriculado { get; set; }
+
+    [Required(ErrorMessage = "La fecha es requerida.")]
+    public DateTime? Fecha { get; set; }
+
+    [Required(ErrorMessage = "La carrera es requerida.")]
+    public Carrera? SuCarrera { get; set; }
 }
